45330ba3cfcc219a1ef713af373a73245cf0e594
Sergey Zwezdin <sergey@zwezdin.com>
2020-05-08 13:23:29 +0500

    feat: Cover validation update

--- index.js
+++ index.js
@@ -67,14 +67,41 @@ hexo.config.covers = Object.assign(
 );
 
 if (hexo.config.covers.enable) {
-    // Process posts, write covers to cache folder
-    hexo.extend.filter.register('before_generate', require('./lib/coversInvalidateFilter')(hexo));
+    /**
+     * Process covers and write to cache folder
+     * when post content or file(s) changed - what happens first.
+     *
+     * ⚠️ Note: if covers already validated in the current cycle,
+     * it won't be runned again until "after_generate".
+     */
+
+    const invalidateCovers = require('./lib/invalidateCovers')(hexo);
+    const injectMetadata = require('./lib/injectMetadata')(hexo);
+    let coversInvaidated = false;
+
+    hexo.extend.filter.register('before_generate', function () {
+        if (coversInvaidated === false) {
+            coversInvaidated = true;
+            return invalidateCovers();
+        }
+    });
+
+    // Inject image cache manifest into page metadata. Process images before if needed.
+    hexo.extend.filter.register('before_post_render', function (data) {
+        if (coversInvaidated === false) {
+            coversInvaidated = true;
+            return invalidateCovers().then(() => injectMetadata(data));
+        } else {
+            return injectMetadata(data);
+        }
+    });
+
+    hexo.extend.filter.register('after_generate', function () {
+        coversInvaidated = false;
+    });
 
     // Inlcude processed images into final output
     hexo.extend.generator.register('covers', require('./lib/coversGenerator')(hexo));
-
-    // Inject image cache manifest into page metadata
-    hexo.extend.filter.register('before_post_render', require('./lib/postMetadataFilter')(hexo));
 
     // Helper to resolve cover from the current post context
     hexo.extend.helper.register('resolve_cover', require('./lib/resolveCoverHelper')(hexo));
